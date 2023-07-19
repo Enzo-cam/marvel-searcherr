a048d464a8152acd401e1d1a18016c4e80d23529
EnzoCam7 <camerac09@gmail.com>
2023-07-19 13:56:10 -0300

    Feat: navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import {
+  Nav,
+  NavLink,
+//   NavMenu,
+  NavSearch,
+  InputSearch,
+  NavBtn,
+//   NavTheme,
+  NavBtnLink,
+  NavHidden,
+  NavHiddenSearch,
+  NavHiddenInput,
+} from "./style";
+import marvelSvg from "../../assets/marvel.svg";
+import {BsStar} from 'react-icons/bs'
+import {BsFillMoonFill} from 'react-icons/bs'
+import {AiOutlineSearch} from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavLink to="/">
+        <img src={marvelSvg} alt="marvelLogo" height="50%" />
+      </NavLink>
+      {/* <NavMenu>
+        <form className="search" onSubmit={onSubmit}>
+          <NavSearch mode={theme}>
+            <i className="fas fa-search" onClick={onSubmit} />
+            <InputSearch>
+              <input
+                type="text"
+                placeholder="Search"
+                onChange={(event) => setText(event.target.value)}
+                value={text}
+              />
+            </InputSearch>
+          </NavSearch>
+        </form>
+      </NavMenu> */}
+      <NavSearch>
+        <AiOutlineSearch />
+        <InputSearch placeholder="Busque aqui su personaje de marvel" />
+      </NavSearch>
+      <NavBtn>
+        <NavBtnLink to="/favorites">
+            <BsFillMoonFill />
+        </NavBtnLink>
+        <NavBtnLink to="/favorites">
+            <BsStar />
+        </NavBtnLink>
+      </NavBtn>
+      <NavHidden>
+        <NavHiddenSearch>
+          <NavHiddenInput placeholder="Search" />
+        </NavHiddenSearch>
+      </NavHidden>
+    </Nav>
+  );
+};
+
+export default Navbar;
